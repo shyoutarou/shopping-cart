8639fc33ae5b50d48ec67b193ee2c1eebef4f5c5
Thiago de Almeida Simões <thiagoalsimoes@gmail.com>
2020-03-14 22:04:24 -0300

    First version of shopping cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import "./App.css";
+import { ProductsList } from "./views/Products";
+import { Product } from "./views/Product";
+import { Store } from "./store";
+import { Cart } from "./views/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={Store}>
+      <div id="main-app">
+        <h1>Amazing Store</h1>
+        <BrowserRouter>
+          <Route exact path="/" component={ProductsList} />
+          <Route path="/product/:id" component={Product} />
+          <Route path="/cart" component={Cart} />
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
